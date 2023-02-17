402b7b45ee869d1e7a00dc02ee74b797a3776776
ladycosy <116603761+ladycosy@users.noreply.github.com>
2023-02-17 14:32:29 +0000

    Navbar styling and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
   return (
-    <div className="navbar">
-      {/* button used for expanding the navbar on a smaller screen */}
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      {/* Button used for expanding the navbar on a smaller screen */}
       <div className="toggleButton">
-        <button></button>
+        {/* Function inside the button to set expand state when the button is clicked  */}
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          {/* Adding the menu icon for mobile screen */}
+          <ReorderIcon />
+        </button>
       </div>
       <div className="links">
-        <Link to={`/`}>Home</Link>
-        <Link to={`/Projects`}>Projects</Link>
-        <link to={`/Experience`}>Experience</link>
-        <link to={`/Contact`}>Contact</link>
+        <Link to="/">Home</Link>
+        <Link to="/Projects">Projects</Link>
+        <Link to="/Experience">Experience</Link>
+        <Link to="/Contact">Contact</Link>
       </div>
     </div>
   );
