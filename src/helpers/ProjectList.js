dafdbdf59141f2950ce69f5398777d39856b8f88
ladycosy <116603761+ladycosy@users.noreply.github.com>
2023-02-18 03:05:51 +0000

    Projects section

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -6,28 +6,34 @@ import P5 from "../assets/P5.png";
 import Figma from "../assets/Figma.png";
 
 export const ProjectList = [
-{
+  {
     name: "BootStrap Portfolio",
-    image: P1 ,
-},
-{
+    image: P1,
+    skills: "JavaScript, HTML",
+  },
+  {
     name: "Password Generator",
     image: P2,
-},
-{
+    skills: "JavaScript, HTML",
+  },
+  {
     name: "Work Day Scheduler",
     image: P3,
-},
-{
+    skills: "JavaScript, HTML",
+  },
+  {
     name: "Weather Dashboard",
     image: P4,
-},
-{
+    skills: "JavaScript, HTML",
+  },
+  {
     name: "Big Night In",
     image: P5,
-},
-{
+    skills: "JavaScript, HTML",
+  },
+  {
     name: "Figma Wireframes",
     image: Figma,
-}
-]
+    skills: "JavaScript, HTML",
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-// import P1 from "../assets/P1.png";
-// import P2 from "../assets/P2.png";
-// import P3 from "../assets/P3.png";
-// import P4 from "../assets/P4.png";
-// import P5 from "../assets/P5.png";
-// import Figma from "../assets/Figma.png";
 import "../styles/Projects.css";
 import { ProjectList } from "../helpers/ProjectList";
 
@@ -14,16 +8,11 @@ function Projects() {
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        {ProjectList.map((project) => {
-          return <ProjectItem name={project.name} image={project.image} />;
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
         })}
-
-        {/* <ProjectItem name="BootStrap Portfolio" image={P1} />
-        <ProjectItem name="Password Generator" image={P2} />
-        <ProjectItem name="Work Day Scheduler" image={P3} />
-        <ProjectItem name="Weather Dashboard" image={P4} />
-        <ProjectItem name="Big Night In" image={P5} />
-        <ProjectItem name="Figma Wireframes" image={Figma} /> */}
       </div>
     </div>
   );
